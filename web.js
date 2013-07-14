b2ed5b003f6a585be00c071873b36898adb9f96a
Vanadium78 <snandy@nandy.com>
2013-07-14 10:59:31 +0000

    utf-8 encoding

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html, encoding));
+  response.send(fs.readFileSync(index.html, encoding='utf-8'));
 });
 
 var port = process.env.PORT || 5000;
